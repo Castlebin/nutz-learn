a6dcdebeaf625f90ebc4da59b5b5fb8fbba62576
Castlebin <Castlebin@gmail.com>
2016-01-04 13:38:15 +0800

    在初始化代码中添加加入默认的管理账户账户信息代码

--- nutzbook/src/main/java/com/heller/nutzbook/MainSetup.java
+++ nutzbook/src/main/java/com/heller/nutzbook/MainSetup.java
@@ -1,10 +1,13 @@
 package com.heller.nutzbook;
 
+import com.heller.nutzbook.bean.User;
 import org.nutz.dao.Dao;
 import org.nutz.dao.util.Daos;
 import org.nutz.ioc.Ioc;
 import org.nutz.mvc.NutConfig;
 import org.nutz.mvc.Setup;
+
+import java.util.Date;
 
 public class MainSetup implements Setup {
     @Override
@@ -12,6 +15,18 @@ public class MainSetup implements Setup {
         Ioc ioc = conf.getIoc();
         Dao dao = ioc.get(Dao.class);
         Daos.createTablesInPackage(dao, "com.heller.nutzbook", false);
+
+        // 初始化，新建默认的根用户
+        if (dao.count(User.class) == 0) {
+            User user = new User();
+            user.setName("admin");
+            user.setPassword("123456");
+            Date now = new Date();
+            user.setCreateTime(now);
+            user.setUpdateTime(now);
+
+            dao.insert(user);
+        }
     }
 
     @Override
