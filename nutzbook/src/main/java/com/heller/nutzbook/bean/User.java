b8bd7d27122de45e4fca66134e408adc878d61ea
Castlebin <Castlebin@gmail.com>
2016-01-04 17:03:57 +0800

    增加log4j日志；修改User bean

--- nutzbook/src/main/java/com/heller/nutzbook/bean/User.java
+++ nutzbook/src/main/java/com/heller/nutzbook/bean/User.java
@@ -1,6 +1,7 @@
 package com.heller.nutzbook.bean;
 
 import lombok.Data;
+import org.nutz.dao.entity.annotation.Column;
 import org.nutz.dao.entity.annotation.Id;
 import org.nutz.dao.entity.annotation.Name;
 import org.nutz.dao.entity.annotation.Table;
@@ -17,12 +18,16 @@ public class User {
     @Name
     private String name;
 
+    @Column("password") // 有一个属性加了@Column注解，必须所有的字段都加（加了@Id、@Name的可以不加）
     private String password;
 
+    @Column("salt")
     private String salt;
 
+    @Column("create_time")
     private Date createTime;
 
+    @Column("update_time")
     private Date updateTime;
 
 }
