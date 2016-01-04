5a59df2bb00e823c025ddfb30a4be2547e85a11f
Castlebin <Castlebin@gmail.com>
2016-01-04 13:26:31 +0800

    配置IocBy，使nutz启动时知道去读取dao.js中的ioc配置

--- nutzbook/src/main/java/com/heller/nutzbook/MainModule.java
+++ nutzbook/src/main/java/com/heller/nutzbook/MainModule.java
@@ -1,7 +1,14 @@
 package com.heller.nutzbook;
 
+import org.nutz.mvc.annotation.IocBy;
 import org.nutz.mvc.annotation.Modules;
+import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
+@IocBy(type = ComboIocProvider.class, args = {
+        "*js", "ioc/",
+        "*anno", "com.heller.nutzbook",
+        "*tx"
+})
 @Modules(scanPackage = true)
 public class MainModule {
 }
