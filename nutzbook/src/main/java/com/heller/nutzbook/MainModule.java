c3c0303d41fb5b3e57fcfe1571820013fe2ea0a3
Castlebin <Castlebin@gmail.com>
2016-01-04 18:25:21 +0800

    使用nutz的动作链，非常强大！So powerfullgit add .git add .

--- nutzbook/src/main/java/com/heller/nutzbook/MainModule.java
+++ nutzbook/src/main/java/com/heller/nutzbook/MainModule.java
@@ -3,6 +3,7 @@ package com.heller.nutzbook;
 import org.nutz.mvc.annotation.*;
 import org.nutz.mvc.ioc.provider.ComboIocProvider;
 
+@ChainBy(args="mvc/nutzbook-mvc-chain.js")
 @Localization(value="msg/", defaultLocalizationKey="zh-CN")
 @Ok("json:full")
 @Fail("jsp:jsp.500")
