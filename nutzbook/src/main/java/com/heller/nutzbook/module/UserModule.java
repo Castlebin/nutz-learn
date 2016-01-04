db1f91596abd9eaf7af93a5228686bf5fe61bc67
Castlebin <Castlebin@gmail.com>
2016-01-04 13:55:42 +0800

    增加登录方法

--- nutzbook/src/main/java/com/heller/nutzbook/module/UserModule.java
+++ nutzbook/src/main/java/com/heller/nutzbook/module/UserModule.java
@@ -1,12 +1,16 @@
 package com.heller.nutzbook.module;
 
 import com.heller.nutzbook.bean.User;
+import org.nutz.dao.Cnd;
 import org.nutz.dao.Dao;
 import org.nutz.ioc.loader.annotation.Inject;
 import org.nutz.ioc.loader.annotation.IocBean;
 import org.nutz.mvc.annotation.At;
 import org.nutz.mvc.annotation.Fail;
 import org.nutz.mvc.annotation.Ok;
+import org.nutz.mvc.annotation.Param;
+
+import javax.servlet.http.HttpSession;
 
 @IocBean
 @At("user")
@@ -20,6 +24,17 @@ public class UserModule {
     @At
     public int count() {
         return dao.count(User.class);
+    }
+
+    @At
+    public Object login(@Param("username") String name, @Param("password") String password, HttpSession session) {
+        User user = dao.fetch(User.class, Cnd.where("name", "=", name).and("password", "=", password));
+        if (user == null) {
+            return false;
+        } else {
+            session.setAttribute("me", user.getId());
+            return true;
+        }
     }
 
 }
